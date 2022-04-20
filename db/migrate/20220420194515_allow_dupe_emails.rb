096c95182eeb22cee978d3a18438396774d9d9d4
Scott Miller <scottmiller@digitalocean.com>
2022-04-20 13:49:41 -0600

    Allow duplicate emails

---
+++ db/migrate/20220420194515_allow_dupe_emails.rb
@@ -0,0 +1,6 @@
+class AllowDupeEmails < ActiveRecord::Migration[6.1]
+  def change
+    remove_index :users, :email
+    add_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_190304) do
+ActiveRecord::Schema.define(version: 2022_04_20_194515) do
 
   create_table "configs", force: :cascade do |t|
     t.string "resource_uuid"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2022_04_18_190304) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 

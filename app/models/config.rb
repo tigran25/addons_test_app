39efa6e49372d7ce2f642ef759b5aeba51497b09
Scott Miller <scottmiller@digitalocean.com>
2022-04-22 11:21:10 -0600

    Update connector version, use conditional on config

--- app/models/config.rb
+++ app/models/config.rb
@@ -2,19 +2,23 @@ class Config < ApplicationRecord
   after_save do
     vars = {}
     customer = DoAddonConnector::Customer.find_by(key: self.resource_uuid)
-    vars.store(self.name, self.value)
+    if customer.present?
+        vars.store(self.name, self.value) 
 
-    # send it to the service
-    customer.update_config(vars)
+        # send it to the service
+        customer.update_config(vars)
+    end
   end
 
   after_destroy do
     vars = {}
     customer = DoAddonConnector::Customer.find_by(key: self.resource_uuid)
-    vars.store(self.name, self.value)
+    if customer.present?
+        vars.store(self.name, self.value)
 
-    # send it to the service
-    customer.update_config(vars)
+        # send it to the service
+        customer.update_config(vars)
+    end
   end
 
 end
